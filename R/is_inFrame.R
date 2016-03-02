8c36b5545e5df557ab2f3e84c31af6c128a007b7
genomewalker <genomewalker@gmail.com>
2016-03-02 17:30:04 +0100

    Minor changes

--- R/is_inFrame.R
+++ R/is_inFrame.R
@@ -1,10 +1,14 @@
 #' @export
 is_inFrame <- function(X){
-  if ((class(X) == "DNAbin" || class(X) == "data.frame")){
-    if (dim(X)[1] %% 3 != 0){
-      stop("Looks like your sequences is not in the correct frame. Please predict the ORFs on your sequence and provide the correct nuclotide file", call. = FALSE)
-    }
-  }else{
+  if ((class(X) != "DNAbin" && class(X) != "data.frame")){
     stop("Wrong class. Should be a DNAbin object for the alignment or a data.frame for the entropy", call. = FALSE)
+  }
+  
+  if (class(X) == "data.frame" & dim(X)[1] %% 3 != 0){
+    stop("Looks like your sequences is not in the correct frame. Please predict the ORFs on your sequence and provide the correct nuclotide file", call. = FALSE)
+  }
+  
+  if (class(X) == "DNAbin" & dim(X)[2] %% 3 != 0){
+    stop("Looks like your sequences is not in the correct frame. Please predict the ORFs on your sequence and provide the correct nuclotide file", call. = FALSE)
   }
 }
