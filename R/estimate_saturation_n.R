becfd8af03cb9da9e0ca6ae026f018aa302666cd
genomewalker <genomewalker@gmail.com>
2016-03-02 11:58:38 +0100

    No lazy data loading

--- R/estimate_saturation_n.R
+++ R/estimate_saturation_n.R
@@ -50,9 +50,7 @@ estimate_saturation_n<-function(aln = aln, rep = 100, nseqs = 1000, model = "K80
     if ((is.null(conf_file)) || (is.null(reg_dir)) || (is.null(reg_id))){
       stop("Please add the configuration file and registry values requiered for BatchJobs" , call. = FALSE)
     }
-    
-    options(BatchJobs.clear.function.env = TRUE)
-    
+        
     function_args <- list()
     fun <- estimate_saturation
     function_args <- list(aln = aln, nseqs = nseqs, model = model, all = all, 
@@ -71,9 +69,11 @@ estimate_saturation_n<-function(aln = aln, rep = 100, nseqs = 1000, model = "K80
     estimate_submission <- BatchJobs::submitJobs(reg, resources=job_res)
     estimate_run <- BatchJobs::waitForJobs(reg, id)
     #estimate_runs <- reduceResultsList(reg)
-    estimate_runs<-lapply(1:rep, function(x, ...){
+    cat("Gathering results...\n")
+    estimate_runs <- plyr::llply(1:rep, function(x, ...){
       res <- BatchJobs::loadResult(reg = reg , id = x)
-      }, reg)
+      }, reg, .parallel = FALSE, .progress = plyr::progress_text(width = 80, char = "+"))
+    
     showStatus(reg)
     removeRegistry(reg, ask = "no")
     
@@ -84,7 +84,8 @@ estimate_saturation_n<-function(aln = aln, rep = 100, nseqs = 1000, model = "K80
   }else{
     
     estimate_runs <- plyr::llply(1:rep, estimate_saturation, aln = aln, nseqs = nseqs, model = model, all = all, verbose = FALSE,
-                             seed = 0, save_aln = save_aln, dir =  dir, .parallel = FALSE, .progress = plyr::progress_text(width = 80), .rsamp = TRUE)
+                             seed = 0, save_aln = save_aln, dir =  dir, .parallel = FALSE, 
+                             .progress = plyr::progress_text(width = 80, char = "+"), .rsamp = TRUE)
   }
   
   results <-list(
